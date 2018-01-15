8017073a1482f5513356e121557aaca020b1c26c
kally <188963758@qq.com>
2018-01-15 19:09:14 +0800

    m

---
+++ src/js/component/header.js
@@ -0,0 +1,19 @@
+/*
+* @Author: Administrator
+* @Date:   2018-01-15 16:36:37
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-01-15 18:33:36
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Header extends React.Component{
+	render(){
+		return (
+			<header>I am the Header!</header>
+		)
+	}
+}
+
+export default Header;
--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,25 @@
 * @Author: Administrator
 * @Date:   2018-01-09 22:15:10
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-01-10 21:59:26
+* @Last Modified time: 2018-01-15 19:05:51
 */
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+import Header from './component/header.js';
+import Body from './component/body.js';
 
-ReactDOM.render(
-    <h1>Hello World.</h1>,
-    document.getElementById('test')
-);
+
+class Index extends React.Component{
+	render(){
+		return (
+			<div>
+				<Header/>
+				<Body />
+				<h2>this is h1</h2>
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<Index/>, document.getElementById('test'));
