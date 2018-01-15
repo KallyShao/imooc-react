aa1af3e3feb33a787a7391f4c2ac44f30d3fa690
KallyShao <188963758@qq.com>
2018-01-15 23:02:28 +0800

    m

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,20 @@
 * @Author: Administrator
 * @Date:   2018-01-09 22:15:10
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-01-15 19:05:51
+* @Last Modified time: 2018-01-15 22:41:01
 */
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-import Header from './component/header.js';
-import Body from './component/body.js';
+// import Header from './component/header.js';
+import BodyIndex from './component/bodyindex.js';
 
 
 class Index extends React.Component{
 	render(){
 		return (
 			<div>
-				<Header/>
-				<Body />
+				<BodyIndex/>
 				<h2>this is h1</h2>
 			</div>
 		)
