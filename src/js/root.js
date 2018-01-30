43ffba18af3ead6b4a85983a094e77f41c199391
KallyShao <188963758@qq.com>
2018-01-30 22:48:38 +0800

    m

--- src/js/root.js
+++ src/js/root.js
@@ -2,29 +2,25 @@
  * @Author: Administrator
  * @Date:   2018-01-29 21:35:53
  * @Last Modified by:   Administrator
- * @Last Modified time: 2018-01-29 22:13:14
+ * @Last Modified time: 2018-01-30 22:45:25
  */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Index from './index.js';
-import List from './component/list.js';
 import {
     Router,
     Route,
     hashHistory
 } from 'react-router';
 
-export default class Root extends React.Component() {
+
+export default class Root extends React.Component {
     render() {
         return (
-            <Router history='hashHistory'>
-                <Route component={Index} path='/'></Route>
-                <Route component={List} path='list'></Route>
-            </Router>
+            <div>Init</div>
         );
     }
 }
 
 
-ReactDOM.render(<Root />, document.getElementById('test'));
+ReactDOM.render(<Root />, document.getElementById('mainContainer'));
