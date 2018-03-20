16ae4afbf4f54af46e29ee5bcaab16b89e76922c
Kally Shao <188963758@qq.com>
2018-03-20 17:48:55 +0800

    移动端注册功能开发，报错

--- src/js/component/mobile_header.js
+++ src/js/component/mobile_header.js
@@ -2,26 +2,116 @@
 * @Author: Kally Shao
 * @Date:   2018-03-15 16:23:57
 * @Last Modified by:   Kally Shao
-* @Last Modified time: 2018-03-16 14:48:27
+* @Last Modified time: 2018-03-20 17:43:15
 */
 
 import React from 'react';
-import { Row, Col, Menu, Icon } from 'antd';
+import { Row, Col, Menu, Icon, Button, Tabs, message, Form, Input, CheckBox, Modal, Link } from 'antd';
 
-export default class MobileHeader extends React.Component {
+//用于表单提交
+const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
+
+class MobileHeader extends React.Component {
     constructor() {
         super();
         this.state = {
-            current: 'top'
+            current: 'top',
+            modalVisible: false,
+            action: 'login',
+            hasLogined: false,
+            userNickName: '',
+            userid: 0
         }
     }
+    ;
+    handleSubmit(e) {
+        //提交数据，注册或登录
+        e.preventDefault(); //阻止事件冒泡
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        var formData = this.props.form.getFieldsValue();
+        // console.log(formData);
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName=" + formData.r_userName + "&r_password=" + formData.r_password + "&r_confirmPassword=" + formData.r_passwordConfirm, myFetchOptions)
+            .then(response => response.json()) //response是借口返回的内容，相当于回调函数中的传参
+            .then(json => {
+                this.setState({
+                    userNickName: json.NickUserName, //由api返回
+                    uerid: json.UserId
+                });
+                message.success('请求成功');
+                this.setModalVisible(false);
+            })
+    }
+    ;
+    handleClick(e) {
+        if (e.key == 'login') {
+            this.setState({
+                current: 'login'
+            });
+            this.setModalVisible(true);
+        } else {
+            this.setState({
+                current: e.key
+            });
+        }
+    }
+    ;
+    setModalVisible(value) {
+        this.setState({
+            modalVisible: value
+        })
+    }
+    ;
+    login() {}
+    ;
     render() {
+        //定义一个全局变量来接收form表单的参数
+        let {getFieldDecorator} = this.props.form;
+        const userShow = !this.state.hasLogined
+            ?
+            <Link>
+                <Icon type="inbox"></Icon>
+            </Link>
+            :
+            <Link>
+                <Icon type="settng" onClick = {this.login.bind(this)}></Icon>
+            </Link>;
+
         return (
             <header id="mobile-header">
-      			<img src="./src/images/news_icon.png" alt="logo"/>
-      			<span>ReactNews</span>
-      		</header>
+                <img src="./src/images/news_icon.png" alt="logo"/>
+                <span>ReactNews</span>
+                {userShow}
+
+                <Modal title="用户中心"
+            wrapClassName="vertical-center-modal"
+            visible={this.state.modalVisible}
+            onCancel= {() => this.setModalVisible(false)}
+            onOk = {() => this.setModalVisible(false)}
+            okText = "关闭"
+            >
+                    <Tabs type="card">
+                      <TabPane tab="注册">
+                        <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                          <FormItem label="账户">
+                            {getFieldDecorator('r_userName')(<Input placeholder="请输入您的账号"></Input>)}
+                          </FormItem>
+                          <FormItem label="密码">
+                            {getFieldDecorator('r_password')(<Input type="password" placeholder="请输入您的密码"></Input>)}
+                          </FormItem>
+                          <FormItem label="确认密码">
+                            {getFieldDecorator('r_passwordConfirm')(<Input type="password" placeholder="请再次输入密码"></Input>)}
+                          </FormItem>
+                          <Button type="primary" htmlType="submit">注册</Button>
+                        </Form>
+                      </TabPane>
+                    </Tabs>
+                </Modal>
+            </header>
         );
     }
     ;
 }
+export default MobileHeader = Form.create({})(MobileHeader);
--- src/js/component/pc_header.js
+++ src/js/component/pc_header.js
@@ -2,7 +2,7 @@
 * @Author: Administrator
 * @Date:   2018-03-15 16:23:38
 * @Last Modified by:   Kally Shao
-* @Last Modified time: 2018-03-16 17:21:27
+* @Last Modified time: 2018-03-20 17:44:23
 */
 
 import React from 'react';
@@ -24,12 +24,44 @@ class PCHeader extends React.Component {
             userid: 0
         }
     }
-    handleSubmit() {}
+    ;
+    handleSubmit(e) {
+        //提交数据，注册或登录
+        e.preventDefault(); //阻止事件冒泡
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        var formData = this.props.form.getFieldsValue();
+        // console.log(formData);
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName=" + formData.r_userName + "&r_password=" + formData.r_password + "&r_confirmPassword=" + formData.r_passwordConfirm, myFetchOptions)
+            .then(response => response.json()) //response是借口返回的内容，相当于回调函数中的传参
+            .then(json => {
+                this.setState({
+                    userNickName: json.NickUserName, //由api返回
+                    uerid: json.UserId
+                });
+                message.success('请求成功');
+                this.setModalVisible(false);
+            })
+    }
+    ;
+    handleClick(e) {
+        if (e.key == 'login') {
+            this.setState({
+                current: 'login'
+            });
+            this.setModalVisible(true);
+        } else {
+            this.setState({
+                current: e.key
+            });
+        }
+    }
     ;
     setModalVisible(value) {
-        this.state = {
+        this.setState({
             modalVisible: value
-        }
+        })
     }
     ;
     render() {
@@ -38,90 +70,92 @@ class PCHeader extends React.Component {
         const userShow = this.state.hasLogined
             ?
             <Menu.Item key="logout" className="register">
-	    		<Button type="primary">{this.state.userNickName}</Button>
-	    		<Link target="_blank">
-	    			<Button type="dashed" htmlType="button">个人中心</Button>
-	    		</Link>
-	    		<Button type="ghost">退出</Button>
-    		</Menu.Item>
+              <Button type="primary">{this.state.userNickName}</Button>
+              <Link target="_blank">
+                <Button type="dashed" htmlType="button">个人中心</Button>
+              </Link>
+              <Button type="ghost">退出</Button>
+            </Menu.Item>
             :
             <Menu.Item key="login" className="register" onClick={this.setModalVisible.bind(this)}>
-    			<Button type="primary" htmlType="button">登录 / 注册</Button>
-    		</Menu.Item>
+              <Button type="primary" htmlType="button">登录 / 注册</Button>
+            </Menu.Item>;
+
         return (
             <header>
-      			<Row>
-      				<Col span={2}></Col>
-      				<Col span={4}>
-      					<a href="/" className="logo">
-      						<img src="./src/images/news_icon.png" alt="logo"/>
-      						<span>ReactNews</span>
-      					</a>
-      				</Col>
-      				<Col span={16}>
-						<Menu mode="horizontal" selectedKeys={[this.state.current]}>
-							<Menu.Item key="top">
-								<Icon type="appstore"></Icon>
-								头条
-							</Menu.Item>
-							<Menu.Item key="shehui">
-								<Icon type="appstore"></Icon>
-								社会
-							</Menu.Item>
-							<Menu.Item key="guonei">
-								<Icon type="appstore"></Icon>
-								国内
-							</Menu.Item>
-							<Menu.Item key="guoji">
-								<Icon type="appstore"></Icon>
-								国际
-							</Menu.Item>
-							<Menu.Item key="yule">
-								<Icon type="appstore"></Icon>
-								娱乐
-							</Menu.Item>
-							<Menu.Item key="tiyu">
-								<Icon type="appstore"></Icon>
-								体育
-							</Menu.Item>
-							<Menu.Item key="keji">
-								<Icon type="appstore"></Icon>
-								科技
-							</Menu.Item>
-							<Menu.Item key="shishang">
-								<Icon type="appstore"></Icon>
-								时尚
-							</Menu.Item>
-							{userShow}
-						</Menu>
-						<Modal title="用户中心"
+            <Row>
+              <Col span={2}></Col>
+              <Col span={4}>
+                <a href="/" className="logo">
+                  <img src="./src/images/news_icon.png" alt="logo"/>
+                  <span>ReactNews</span>
+                </a>
+              </Col>
+              <Col span={16}>
+                <Menu mode="horizontal" selectedKeys={[this.state.current]} onClick={this.handleClick.bind(this)}>
+                  <Menu.Item key="top">
+                    <Icon type="appstore"></Icon>
+                    头条
+                  </Menu.Item>
+                  <Menu.Item key="shehui">
+                    <Icon type="appstore"></Icon>
+                    社会
+                  </Menu.Item>
+                  <Menu.Item key="guonei">
+                    <Icon type="appstore"></Icon>
+                    国内
+                  </Menu.Item>
+                  <Menu.Item key="guoji">
+                    <Icon type="appstore"></Icon>
+                    国际
+                  </Menu.Item>
+                  <Menu.Item key="yule">
+                    <Icon type="appstore"></Icon>
+                    娱乐
+                  </Menu.Item>
+                  <Menu.Item key="tiyu">
+                    <Icon type="appstore"></Icon>
+                    体育
+                  </Menu.Item>
+                  <Menu.Item key="keji">
+                    <Icon type="appstore"></Icon>
+                    科技
+                  </Menu.Item>
+                  <Menu.Item key="shishang">
+                    <Icon type="appstore"></Icon>
+                    时尚
+                  </Menu.Item>
+                  {userShow}
+                </Menu>
+            <Modal title="用户中心"
             wrapClassName="vertical-center-modal"
             visible={this.state.modalVisible}
             onCancel= {() => this.setModalVisible(false)}
             onOk = {() => this.setModalVisible(false)}
             okText = "关闭"
             >
-            				<Tabs type="card">
-            					<TabPane tab="注册">
-            						<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
-            							<FormItem label="账户">
-	            							{getFieldDecorator('r_userName')(<Input placeholder="请输入您的账号"></Input>)};
-            							</FormItem>
-            							<FormItem label="密码">
-	            							{getFieldDecorator('r_password')(<Input placeholder="请输入您的密码"></Input>)};
-            							</FormItem>
-            							<FormItem label="确认密码">
-	            							{getFieldDecorator('r_passwordConfirm')(<Input placeholder="请再次输入密码"></Input>)};
-            							</FormItem>
-            						</Form>
-            					</TabPane>
-            				</Tabs>
-            			</Modal>
-      				</Col>
-      				<Col span={2}>
-      				</Col>
-      			</Row>
-      		</header>
+                    <Tabs type="card">
+                      <TabPane tab="注册">
+                        <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                          <FormItem label="账户">
+                            {getFieldDecorator('r_userName')(<Input placeholder="请输入您的账号"></Input>)}
+                          </FormItem>
+                          <FormItem label="密码">
+                            {getFieldDecorator('r_password')(<Input type="password" placeholder="请输入您的密码"></Input>)}
+                          </FormItem>
+                          <FormItem label="确认密码">
+                            {getFieldDecorator('r_passwordConfirm')(<Input type="password" placeholder="请再次输入密码"></Input>)}
+                          </FormItem>
+                          <Button type="primary" htmlType="submit">注册</Button>
+                        </Form>
+                      </TabPane>
+                    </Tabs>
+                  </Modal>
+              </Col>
+              <Col span={2}>
+              </Col>
+            </Row>
+          </header>
         );
     }
     ;
